bab5243db7d37f6803fab9bf9eba736fd8ba75dc
ArtificialTruth <thorvald.soerensen@protonmail.com>
2022-10-05 19:03:24 +0200

    Implemtented  average calculation on city records as sep. function. Added getters and setters for CityRecord

--- Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
@@ -3,46 +3,108 @@ package edu.sdse.csvprocessor;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
+
+/* Java HashMap sneak peak:
+ * {Copenhagen=[Id: 1, Year: 2003, City: Copenhagen, Population: 501285, Id: 4, Year: 2004, City: Copenhagen, Population: 501664, ...],
+ * Oslo=[Id: 3, Year: 2012, City: Oslo, Population: 618683, Id: 7, Year: 2008, City: Oslo, Population: 567980, ....
+ */
 
 public class CityCSVProcessor {
-	
+
 	public void readAndProcess(File file) {
 		//Try with resource statement (as of Java 8)
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-			//Discard header row
+			//Extract header row
 			String[] header = br.readLine().trim().split(",");
 			new CityRecord(header); // Make header shared for all instances of CityRecord
 
+			ArrayList<CityRecord> allRecords = new ArrayList<CityRecord>();
+
 			String line;
+
+			HashMap< String, ArrayList<CityRecord> > cityDict = new HashMap<>();
 			
 			while ((line = br.readLine()) != null) {
 				// Parse each line
 				String[] rawValues = line.split(",");
 				
-				int id = convertToInt(rawValues[0]);
-				int year = convertToInt(rawValues[1]);
+				int id = convertToint(rawValues[0]);
+				int year = convertToint(rawValues[1]);
 				String city = convertToString(rawValues[2]);
-				int population = convertToInt(rawValues[3]);
+				int population = convertToint(rawValues[3]);
 
 				CityRecord cr = new CityRecord(id, year, city, population);
 				
-				System.out.println(cr);
-				//System.out.println("id: " + id + ", year: " + year + ", city: " + city + ", population: " + population);
+				// System.out.println(cr);
+				allRecords.add(cr);
 				
-				//TODO: Extend the program to process entries!
+				// use map to colllect records by their city
+				// HashMap is similiar to Python Dictionary
+				/* { 'copenhagen': [<CityRecord2001>, <CityRecord2002>] } */
+				
+				ArrayList<CityRecord> currentList = cityDict.getOrDefault(city, new ArrayList<CityRecord>());
+				currentList.add(cr);
+				cityDict.put(city, currentList);
+
 			}
+
+			printStatistics(cityDict);
+
+			//For debug: System.out.println(cityDict);
 		} catch (Exception e) {
 			System.err.println("An error occurred:");
 			e.printStackTrace();
 		}
+	}
+
+	private void printStatistics(HashMap< String, ArrayList<CityRecord> > cityDict){
+		// proccess data of each city, a) b) c):
+
+		for (Entry< String, ArrayList<CityRecord> > entry : cityDict.entrySet()) {
+			String city = entry.getKey();
+			ArrayList<CityRecord> cityHistory = entry.getValue();
+
+			int cityEntries = 0;
+			int cityMinYear = 999999999; // Init to high val so first compare will always be smaller
+			int cityMaxYear = -999999999; // Init to low val so first compare will always be larger
+			int cityPopCumSum = 0;
+			int cityAvgPop = 0;
+
+			for (CityRecord record : cityHistory){
+				cityEntries++; // a) the total number of entries for that city
+				int cityYear = record.getYear();
+				
+				if (cityYear < cityMinYear){ // b) the minimum year
+					cityMinYear = cityYear;
+				}
+
+				if (cityYear > cityMaxYear){ // c) the maximum year
+					cityMaxYear = cityYear;
+				}
+
+				// Cummulated sum
+				cityPopCumSum += record.getPopulation();
+			}
+
+			// d) the average population over that period (i.e., the sum of all 
+			// population measurements divided by the total number of entries).
+			// Cast to Int to avoid decimal ppl
+			cityAvgPop = (int) (cityPopCumSum / cityEntries);
+
+			System.out.println("Average population of " + city + "(" + cityMinYear + "-"+ cityMaxYear + "; " + cityEntries + " entries): " + cityAvgPop);
+		}
+
 	}
 	
 	private String cleanRawValue(String rawValue) {
 		return rawValue.trim();
 	}
 	
-	private int convertToInt(String rawValue) {
+	private int convertToint(String rawValue) {
 		rawValue = cleanRawValue(rawValue);
 		return Integer.parseInt(rawValue);
 	}
--- Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityRecord.java
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityRecord.java
@@ -5,14 +5,20 @@ package edu.sdse.csvprocessor;
  * DataClass to store city info
  */
 public class CityRecord {
-  int id;
-  int year;
-  String city;
-  int population; 
+  private int id;
+  private int year;
+  private String city;
+  private int population; 
 
   // Static makes this variable shared across all instances of CityRecord to avoid
   // duplicate mem usage
-  static String[] header; // https://www.tutorialspoint.com/class-and-static-variables-in-java
+  private static String[] header; // https://www.tutorialspoint.com/class-and-static-variables-in-java
+
+  // This will init the header value for all instances of CityRecord
+  // Overloading constructor
+  CityRecord(String[] header){
+    CityRecord.header = header; // Access header variable in a "static way"
+  }
 
   CityRecord(int id, int year, String city, int population){
     this.id=id;
@@ -21,10 +27,24 @@ public class CityRecord {
     this.population=population;
   }
 
-  // This will init the header value for all instances of CityRecord
-  // Overloading constructor
-  CityRecord(String[] header){
-    CityRecord.header = header; // Access header variable in a "static way"
+  public int getId() {
+    return id;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public int getPopulation() {
+    return population;
+  }
+
+  public String[] getHeader() {
+    return header;
   }
 
   @Override
